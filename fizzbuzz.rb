f9bd161b401cc78e75d5a70ea2ebc3db0c9ea820
haleykeizur <haleykz@yahoo.com>
2018-07-24 21:20:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,11 @@
 
 def fizzbuzz(int)
-  if int%3==0
-    "Fizz"
+  if int%3==0 && int%5==0
+    "FizzBuzz"
   elsif int%5==0
     "Buzz"
-  else int%3==0 && int%5==0
-    "FizzBuzz"
-  else
-    nil
+  else int%3==0
+    "Fizz"
   end
 end
 
