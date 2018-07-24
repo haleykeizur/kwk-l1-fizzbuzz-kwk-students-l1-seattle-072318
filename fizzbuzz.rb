7c4f41e5ebce5a1591bd2a93f10422d41ccf7e78
haleykeizur <haleykz@yahoo.com>
2018-07-24 21:10:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,12 @@
-int=33
- 
-if int%15==0
-  puts "FizzBuzz"
-elsif int%5==0
-  puts "Buzz"
-else int%3==0
-  puts "Fizz"
+
+def fizzbuzz(int)
+  if int%15==0
+    puts "FizzBuzz"
+  elsif int%5==0
+    puts "Buzz"
+  else int%3==0
+    puts "Fizz"
+  end
 end
 
+fizzbuzz(33)
