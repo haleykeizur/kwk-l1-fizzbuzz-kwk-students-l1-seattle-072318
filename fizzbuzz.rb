c049a10f01e35eab3f913cc4302c423fbeea556b
haleykeizur <haleykz@yahoo.com>
2018-07-24 21:11:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 
 def fizzbuzz(int)
-  if int%15==0
+  if int%15=0
     puts "FizzBuzz"
-  elsif int%5==0
+  elsif int%5=0
     puts "Buzz"
-  else int%3==0
+  else int%3=0
     puts "Fizz"
   end
 end
